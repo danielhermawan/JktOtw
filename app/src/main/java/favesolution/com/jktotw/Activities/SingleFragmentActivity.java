3cc791a9057281a90eb3c0abc5f565f88f649542
Daniel Hermawan <danielhermawan96@gmail.com>
2015-10-31 04:16:56 +0700

    10/13/2015 4:16 AM

---
+++ app/src/main/java/favesolution/com/jktotw/Activities/SingleFragmentActivity.java
@@ -0,0 +1,46 @@
+package favesolution.com.jktotw.Activities;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewConfiguration;
+
+import java.lang.reflect.Field;
+
+import favesolution.com.jktotw.R;
+
+/**
+ * Created by Daniel on 6/12/2015.
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    public abstract Fragment createFragment();
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if(fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer,fragment).commit();
+        }
+        showOverflowMenu(this);
+    }
+    private void showOverflowMenu(FragmentActivity activity) {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(activity);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if (menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
